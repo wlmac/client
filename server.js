4b3a17cdb9539974b2565788104d2db2c48a2e17
Patrick Lin <apocalypsecalculator@gmail.com>
2022-09-09 21:12:31 -0400

    basic server fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const PORT = 8080;
+const path = require('path');
 const express = require('express');
 const app = express();
 app.enable('trust proxy');
@@ -6,6 +7,10 @@ app.enable('trust proxy');
 app.use('/static', express.static('static'));
 
 app.use(express.static('dist', { extensions: ["html"] }));
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Local server at http://localhost:${PORT}/`);
