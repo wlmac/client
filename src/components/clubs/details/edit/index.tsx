fd1575294a701dfdab38d7ef5ee4cca9b3236f36
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-03-03 21:37:17 -0500

    added textarea to make changes to bio

--- src/components/clubs/details/edit/index.tsx
+++ src/components/clubs/details/edit/index.tsx
@@ -10,7 +10,18 @@ import { TagElement } from "../../../../util/core/tags";
 
 export const EditClubDetails = (): JSX.Element => {
     const { id } = useParams(); 
+    const session: Session = React.useContext(SessionContext); 
     const [club, setClub] = React.useState({} as Organization);
+
+    React.useEffect(() => {
+        const fetchURL = `${Routes.OBJECT}/organization/retrieve/${id}`;
+        session.getAPI(fetchURL).then((res) => {
+            const current_club: Organization = res.data as Organization;
+            setClub(current_club);
+        })
+    }, []);
+
+    console.log(`${club.name}'s description: ${club.bio}`);
 
     return (
         <>
@@ -19,7 +30,16 @@ export const EditClubDetails = (): JSX.Element => {
                 <div className="row">
                     <img className="club-banner responsive-img col s12" src={club.banner} alt="banner of organization" />
                 </div>
-                <p>magic..</p>
+                <div className="container">
+                    <div className="row page-title">
+                        <h1><strong>Edit club details for {club.name}</strong></h1>
+                    </div>
+                    <div className="row edit-club-bio m3">
+                        <textarea>
+                            {club.bio}
+                        </textarea>
+                    </div>
+                </div>
             </div>
         </>
     );
