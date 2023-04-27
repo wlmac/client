92a9f9acd91d57f2f87c7bb843a508e9cef08cdd
Jimmy Liu <jimmyjcl753@gmail.com>
2023-04-27 19:44:20 -0400

    Initial commit for fix ann home feed

--- src/util/core/misc/routes.ts
+++ src/util/core/misc/routes.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 const BASEURL = import.meta.env.DEV ? "https://auth.jimmyliu.dev" : "https://maclyonsden.com";
 // const BASEURL = "https://auth.jimmyliu.dev";
+// const BASEURL = "https://maclyonsden.com";
 
 const Routes = {
   BASEURL: BASEURL,
