992a5588f8ba1132ff4d4f063a04e80310d4eccc
Jimmy Liu <jimmyjcl753@gmail.com>
2023-04-29 13:57:28 -0400

    Fix profile render error 2

--- src/components/account/profile/view/index.tsx
+++ src/components/account/profile/view/index.tsx
@@ -53,10 +53,14 @@ export const ProfileView = (): JSX.Element => {
 
     React.useEffect(() => {
 <<<<<<< HEAD
+<<<<<<< HEAD
         if (!("username" in user)) return;
 =======
         if (!user) return;
 >>>>>>> Fix profile render error
+=======
+        if (!("username" in user)) return;
+>>>>>>> Fix profile render error 2
         const organization_display_list: Array<String> = [];
         session.allOrgs.forEach((organization: Organization) => {
             if (user.organizations.find((element) => element === organization.id)) {
