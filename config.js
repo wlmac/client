1855b379b8bcabb1d46f352ddedb932d27077ce7
Jimmy Liu <jimmyjcl753@gmail.com>
2023-05-01 19:47:24 -0400

    Finalize about our team, resolves #50

---
+++ config.js
@@ -0,0 +1,19 @@
+let config = {
+    METROPOLIS_STAFFS: {
+        "Project Manager": [9],
+        "Frontend Developers": [],
+        "Backend Developers": [],
+        "App Developers": [],
+        "Graphic Designers": [],
+        "Content Creators": [],
+        "Game Developers": [],
+        "Alumnui": []
+    },
+    METROPOLIS_STAFF_BIO: {
+        "1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "2": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "9": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    }
+};
+
+export default config;
