85c3a7fd3b41447c79e2aa252bf42c34dbebc6c9
Daniel Ye <danielyeqt@gmail.com>
2023-07-26 22:08:39 -0400

    typescript compatibility in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/app";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
 root.render(<App />);
